295030eddb60869be3ef1ed70480b8ddec2fb704
Clinton Cecil <umdmariachi@gmail.com>
2017-04-01 23:53:16 -0500

    Uses token in requests, adds all_pulls method

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -1,17 +1,31 @@
 class GithubService
   include HTTParty
   base_uri 'https://api.github.com'
-  @default_headers = {'Accept' => 'application/vnd.github.v3+json'}
 
   def initialize(org="lodash")
     @org = org
+    @default_headers = {'Accept' => 'application/vnd.github.v3+json'}
+    @default_options = {headers: @default_headers, Authorization: "token #{Figaro.env.github_token}"}
   end
 
   def repos
-    self.class.get("/orgs/#{@org}/repos")
+    self.class.get("/orgs/#{@org}/repos", options: @default_options)
   end
 
   def pulls(repo="lodash", state="all", page="1")
-    self.class.get("/repos/#{@org}/#{repo}/pulls?state=#{state}&page=#{page}", options: {headers: @default_headers})
+    self.class.get("/repos/#{@org}/#{repo}/pulls?state=#{state}&page=#{page}", options: @default_options)
+  end
+
+  def all_pulls(repo="lodash", state="all")
+    i = 1
+    has_next = true
+    while has_next do
+      results = pulls(repo, state, i)
+      has_next = results.headers["link"].include? 'rel="next"'
+      results.each do |pull|
+        yield pull
+      end
+      i += 1
+    end
   end
 end
