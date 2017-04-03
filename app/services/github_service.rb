6a2e51467931fa981fe31f0dfb179008ff4c3b69
Clinton Cecil <umdmariachi@gmail.com>
2017-04-02 23:29:28 -0500

    adds org and rate_limit endpoints to github service, as well as calculation for rate limit clearing time. Fixes headers

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -4,16 +4,36 @@ class GithubService
 
   def initialize(org="lodash")
     @org = org
-    @default_headers = {'Accept' => 'application/vnd.github.v3+json'}
-    @default_options = {headers: @default_headers, Authorization: "token #{Figaro.env.github_token}"}
+    @default_headers = {
+      'Accept' => 'application/vnd.github.v3+json',
+      'Authorization' => "token #{Figaro.env.github_token}",
+      'User-Agent' => "#{Figaro.env.github_user}"}
+  end
+
+  def org
+    self.class.get("/orgs/#{@org}", headers: @default_headers)
   end
 
   def repos
-    self.class.get("/orgs/#{@org}/repos", options: @default_options)
+    self.class.get("/orgs/#{@org}/repos", headers: @default_headers)
   end
 
   def pulls(repo="lodash", state="all", page="1")
-    self.class.get("/repos/#{@org}/#{repo}/pulls?state=#{state}&page=#{page}", options: @default_options)
+    self.class.get("/repos/#{@org}/#{repo}/pulls?state=#{state}&page=#{page}", headers: @default_headers)
+  end
+
+  def rate_limit
+    self.class.get("/rate_limit", headers: @default_headers)
+  end
+
+  def time_until_rate_limit_clear
+    response = JSON.parse rate_limit.body
+    if response["resources"]["core"]["remaining"] == 0
+      seconds = response["resources"]["core"]["reset"].to_i - Time.now.to_i + 10
+      return seconds
+    else
+      return 0
+    end
   end
 
   def all_pulls(repo="lodash", state="all")
@@ -21,11 +41,13 @@ class GithubService
     has_next = true
     while has_next do
       results = pulls(repo, state, i)
-      has_next = results.headers["link"].include? 'rel="next"'
-      results.each do |pull|
-        yield pull
+      if results.success?
+        has_next = results.headers["link"].andand.include? 'rel="next"'
+        results.each do |pull|
+          yield pull
+        end
+        i += 1
       end
-      i += 1
     end
   end
 end
